e2294a72c8bdb16acbbe57e90de6022c1099e27e
NaufalF121 <blueshoko@gmail.com>
2024-05-29 17:14:09 +0700

    add koneksi kafka to crypto btc to the moon

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-import requests
-import time
 import os
 import finnhub
-
+import json as json
+import time
+import requests
+from concurrent.futures import ThreadPoolExecutor
+from time import sleep
+from kafka import KafkaProducer
+import sys, types
 
 API_KEY = os.getenv("API_KEY")
 finnhub_client = finnhub.Client(api_key=API_KEY)
 url = 'https://api.coincap.io/v2/assets'
+m = types.ModuleType('kafka.vendor.six.moves', 'Mock module')
+setattr(m, 'range', range)
+sys.modules['kafka.vendor.six.moves'] = m
 
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092']
+    
+)
 params = {
     'search' : 'BTC',
     'limit': 1
@@ -20,7 +31,8 @@ while True:
     print("Cryptocurrency")
     print(data['data'][0]['name'])
     print(data['data'][0]['priceUsd'])
-    print("Stock")
-    print(finnhub_client.quote('PBCRF'))
+    producer.send('test', json.dumps(data['data'][0]).encode('utf-8'))
+    # print("Stock")
+    # print(finnhub_client.quote('PBCRF'))
     time.sleep(31)
     
