ee757c46bf72a9d35439baefb160bf814db14f5f
NaufalF121 <blueshoko@gmail.com>
2024-05-29 16:54:30 +0700

    add bbca stock

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
 import time
 import os
+import finnhub
 
 
-
+API_KEY = os.getenv("API_KEY")
+finnhub_client = finnhub.Client(api_key=API_KEY)
 url = 'https://api.coincap.io/v2/assets'
 
 params = {
@@ -15,7 +17,10 @@ while True:
 
     r = requests.get(url, params=params)
     data = r.json()
+    print("Cryptocurrency")
     print(data['data'][0]['name'])
     print(data['data'][0]['priceUsd'])
+    print("Stock")
+    print(finnhub_client.quote('PBCRF'))
     time.sleep(31)
     
