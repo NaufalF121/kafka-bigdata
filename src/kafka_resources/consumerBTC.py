cdaa52ecfdff939de01e53f1c9c9a8059c1542da
NaufalF121 <blueshoko@gmail.com>
2024-06-04 22:09:05 +0700

    consumerBTC store val in postgres

--- src/kafka_resources/consumerBTC.py
+++ src/kafka_resources/consumerBTC.py
@@ -1,11 +1,14 @@
 from confluent_kafka import Consumer, KafkaException
 import json
 import logging
+from connection import Connection
+import os
 
 # Configuration
 TOPIC = "bitcoin"
 BOOTSTRAP_SERVERS = 'localhost:9092'
 GROUP_ID = 'mygroup'
+QUERY = ""
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -17,7 +20,7 @@ def create_consumer():
         'auto.offset.reset': 'earliest'
     })
 
-def consume_message(consumer):
+def consume_message(consumer,conn):
     while True:
         msg = consumer.poll(1.0)
 
@@ -26,19 +29,25 @@ def consume_message(consumer):
         if msg.error():
             logging.error(f"Consumer error: {msg.error()}")
             continue
-
         try:
             message = json.loads(msg.value().decode('utf-8'))
             logging.info(message)
+            conn.execute(QUERY)
+            
+
         except json.JSONDecodeError:
             logging.error("Failed to decode JSON message")
+            
 
 
 
 def main():
+    conn = Connection(host=os.getenv("HOST"), port=os.getenv("PORT"), database=os.getenv("DB_NAME"), user=os.getenv("USER"), password=os.getenv("PASSWORD"))
+    conn.connect()
     consumer = create_consumer()
     consumer.subscribe([TOPIC])
-    consume_message(consumer)
+    consume_message(consumer, conn=conn)
+    conn.close()
 
 
 if __name__ == "__main__":
