4b6b5c938a19885508174310881698e3e8380618
NaufalF121 <blueshoko@gmail.com>
2024-06-05 12:45:35 +0700

    create migrations

---
+++ src/migration/1244562024_01.sql
@@ -0,0 +1,10 @@
+CREATE TABLE cryptobtc (
+  supply 	double precision,
+  maxsupply 	double precision,
+  marketcapusd24hr double precision,
+  volumeusd24hr double precision,
+  changepercent24hr double precision,
+  vwap24hr double precision,
+  price double precision,
+  timeid timestamp
+)
